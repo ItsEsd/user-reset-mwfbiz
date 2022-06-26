c04eebd38ba3f49839213ce5fe5f4cb6745c6e20
S Das <81977626+ItsEsd@users.noreply.github.com>
2022-06-26 18:38:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var elem = document.getElementById('footer');
+$('#btnobdm').click(function(){
+$('#usrobdm').show();
+$('#secone').hide();
+elem.style.display='none';
+});
+
+$('#btnmono').click(function(){
+    $('#usrmono').show();
+    $('#secone').hide();
+    elem.style.display='none';
+    });
