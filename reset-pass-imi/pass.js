4868460744db7a7448fe49256cafff1b071d82ab
S Das <d.soubhik@hotmail.com>
2023-06-22 00:24:57 +0530

    Update pass.js

--- reset-pass-imi/pass.js
+++ reset-pass-imi/pass.js
@@ -426,30 +426,35 @@ document.getElementById('svnewpass-m').addEventListener('click', updatepassM);
 
 function updatepassM() {
   document.getElementById("loader3-m").style.visibility = "visible";
-  var script_pass = "https://script.google.com/macros/s/AKfycbz53jB8DheXzsXra-yGTIo4tXA7TzjUZGwS0moaWDyB-9Zf6sv5Y8Xxxc2dYz3aumepjQ/exec";
+  var script_pass = "https://script.google.com/macros/s/AKfycbxLuHw6CPKhGb7Fjm-CxEMIRybUn-Tt3y89GB90W6j_gO0Pg7zC3ySQX-FkbspoG6eldg/exec";
   var newP = $('#passwordnew-m').val();
   var emid = $('#email1-m').val();
   if (emid != "info@mwfbiz.com" && newP != 0) {
-    var urlp = script_pass + "?callback=ctrlqup&email1=" + emid + "&passwordnew=" + newP + "&action=cuiweryuiwrnweroumorilewr-werhwekrh34iuy4werhkjry34iy5idhwehrwerw-e4t4hwuegrnewutr8723t4ugruereor-wer3648yrncwniryew87ryneiryb8475wre-rewr485bw8ericweynr8ewwnwehLOIAROWER-ydfhindsfndiushnfdhudsfkdsf-fskdhKGjGKGuKDdsnifskfhshfsdldsf0w979847fhkfhd-uyeinxe87798340092o0347989yhkhkgfd98ghtkgukhgkjfdy89jfdg";
+    var urlp = script_pass + "?callback=bzmnlgudtd&email1=" + emid + "&passwordnew=" + newP + "&action=cuiweryuiwrnweroumorilewr-werhwekrh34iuy4werhkjry34iy5idhwehrwerw-e4t4hwuegrnewutr8723t4ugruereor-wer3648yrncwniryew87ryneiryb8475wre-rewr485bw8ericweynr8ewwnwehLOIAROWER-ydfhindsfndiushnfdhudsfkdsf-fskdhKGjGKGuKDdsnifskfhshfsdldsf0w979847fhkfhd-uyeinxe87798340092o0347989yhkhkgfd98ghtkgukhgkjfdy89jfdg";
     var request = jQuery.ajax({
       crossDomain: true,
       url: urlp,
       method: "GET",
       dataType: "jsonp"
     });
-    document.getElementById("loader3-m").style.visibility = "hidden";
-    document.getElementById('showPass-m').innerHTML = '<div><h4 style="color:red;font-size:16px;"><p style="color:#008000;">&#9745; You have changed your pass successfully</p></div>';
+    
   } else {
     document.getElementById("loader3-m").style.visibility = "hidden";
     document.getElementById('svnewpass-m').disabled = true;
     document.getElementById('showPass-m').innerHTML = '<p style="color:red;">Guest pass cannot be changed</p>';
   }
-  setTimeout(function() {
-    $('#showPass-m').fadeOut('fast');
-    location.reload();
-  }, 10000);
+ 
 }
-
+function bzmnlgudtd(e){
+  if(e.result=="Value updated successfully!"){
+    document.getElementById("loader3-m").style.visibility = "hidden";
+    document.getElementById('showPass-m').innerHTML = '<div><h4 style="color:red;font-size:16px;"><p style="color:#008000;">&#9745; You have changed your pass successfully</p></div>';
+    setTimeout(function() {
+          $('#showPass-m').fadeOut('fast');
+          location.reload();
+        }, 5000);
+      }
+  }
 function verifyEmailM() {
   var ml = $("#mid-m").val();
   if (ml != 0) {
